606ae162e8061a66be717ba5f765646fbdc57c7c
Mathieu Roberts <mathieulroberts@gmail.com>
2022-08-17 00:05:26 -0700

    removed 102 site, made 201 site

--- script.js
+++ script.js
@@ -1,78 +1,61 @@
-// let x = 5
-// let y = 4
-// let z = x + y
-// console.log(z)
+'use strict';
 
+let name = prompt("What is your name?");
+alert("Welcome to my website " + name + ", prepare to guess");
 
-// for getting input
-// const input = prompt("How do it do?");
-// console.log(input);
-// console.log(typeof(input))
-// console.log("but " + input + " how tho?");
-
-//for giving a message
-// alert("This is an alert")
-
-const input = prompt("Welcome to Best Website™, what is your name?");
-document.write(input)
-
-
-const element = document.getElementById("button");
-    if(element){
-        element.addEventListener('click', buttonclick, false);
-    }
-
-
-function buttonclick(){
-    let input = prompt("Enter Giveaway!? y/n")
-    console.log(input)
-    
-    if(input == "y"){
-        window.open("https://www.youtube.com/watch?v=tS_2hEmGnzA&ab_channel=Adroon");
-        return;
-    } 
-    else if(input == "n"){
-        window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley");
-        return;
-    }
-    
-    buttonclick()
-
-} 
-
-
-
-function printMoney(){
-    let x = 0;
-    let input2 = document.getElementById("formInput").value;
-
-    if (isNaN(input2)){
-        alert("Please put in a number");
-
-    }
-    
-    while (x < input2) {
-        const m = '<img src="monie.jpg" alt="monies" width="250" height="100" />'
-        x++;
-        document.getElementById("monies");
-        monies.insertAdjacentHTML("afterend", m);
-
-    }
-
+let color = prompt("What is my favorite color?");
+color = color.toLowerCase();
+switch (color) {
+    case 'crimson':
+        alert('Correct');
+        break;
+    default:
+        alert('Incorrect');
 }
 
-// function burnMoney() {
-//     const b = ""
-//     document.getElementById("monies");
-//     monies.insertAdjacentHTML("beforebegin", b);
-    
-// }
+let movie = prompt('What is my favorite movie?');
+movie = movie.toLowerCase();
+if (movie === 'redline') {
+    alert('Theres no way you actually guessed that, you looked at the code didnt you?');
+} else {
+    alert('Incorrect');
+}
 
+let car = prompt('Do I like older cars? y/n');
+car = car.toLowerCase();
+if (car === 'y') {
+    alert('Absolutley, theres no vehicles that are better')
+} else if (car === 'n') {
+    alert('Wrong and if you dont think so your opinion is also wrong ' + name);
+} else {
+    alert('didnt even try to answer?');
+}
 
+let game = prompt('What is my favorite PC game?');
+game = game.toLowerCase();
+switch (game) {
+    case 'rimworld':
+        alert('Correct, very fun');
+        break;
+    case 'minecraft':
+        alert('Its fun but its not my favorite');
+        break;
+    default:
+        alert('I didnt put a case for that game');
+}
 
+let music = prompt('What is the best genre of music?');
+music = music.toLowerCase();
+switch (music) {
+    case 'rock':
+        alert('Yes!, I love screaming guitars');
+        break;
+    case 'electronic':
+        alert('Sounds nice, but not my go to');
+        break;
+}
 
-
-
+let farewell = alert('Guessing game complete, nice job ' + name + ', enjoy the website now');
 
 
 
